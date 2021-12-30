58dc8d53a03c665cb682926b8176e13dfcdfa674
Sagar Maskawar <92781577+Sagarmaskawar@users.noreply.github.com>
2021-12-30 22:42:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+     <h1 data-ns-test="Project-name">Amazon Clone</h1>
+      <p data-ns-test="Project-discription">i want to make a project on Amazon. </p>
+     <
+    </div>
   )
 }
 
