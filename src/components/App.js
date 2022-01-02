a0c2abdcedf7e760e66917ae34f5ddfb8a449d34
Sagar Maskawar <92781577+Sagarmaskawar@users.noreply.github.com>
2022-01-02 19:49:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,15 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-function Appps(){
-const element= <div> <h1 data-ns-test="project-name">Amazon Clone</h1>
-      <p data-ns-test="project-description">i want to make a project on Amazon. </p>
-     </div>
-}
+
 const App = () => {
+      const project-name=Amazon_Clone;
+      const project-discription= I Love Amazon Thats Why.
+      
   return (
     <div id="main">
-          <Apps/>
+         <div data-ns-test={project-name}></div>
+         <div data-ns-test={project-discription></div>
+         
             
     </div>
   )
