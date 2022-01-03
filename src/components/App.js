2c12cb1a7f8a09ceb514101933b794e53522031a
Sagar Maskawar <92781577+Sagarmaskawar@users.noreply.github.com>
2022-01-03 09:47:46 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,15 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
-      const pro = 'Amazon Clone';
-      const des='I want to create a amazon clone';
+      const pro = "Amazon Clone";
+      const des="I want to create a amazon clone";
       
   return (
     <div id="main">
          <h1 data-ns-test="project-name">{pro}</h1>
          <p data-ns-test="project-discription">{des}</p>
          
-            
-    </div>
+     </div>
 )
 }
 
